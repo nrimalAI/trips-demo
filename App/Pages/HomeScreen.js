0cfd69af470d60ad5203c86b7cc2a2ecc5377c98
Nitant R <54756510+nrimalAI@users.noreply.github.com>
2023-09-24 17:48:01 -0400

    Homescreen

--- App/Pages/HomeScreen.js
+++ App/Pages/HomeScreen.js
@@ -1,21 +1,47 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image, FlatList, TouchableOpacity, Dimensions } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import ChatFaceData from '../Services/ChatFaceData';
 
 export default function HomeScreen() {
     const [chatFaceData, setChatFaceData] = useState(); 
-    const [selectedChatFaceData, setSelectedChatFaceData] = useState();
+    const [selectedChatFaceData, setSelectedChatFaceData] = useState(ChatFaceData[0]);
 
     useEffect(() => {
         setChatFaceData(ChatFaceData);
         setSelectedChatFaceData(ChatFaceData[0])
 
     }, [])
-  return ( 
-     <View>
-      <Text style={[{color:selectedChatFaceData.primary}]}>Hey</Text>
-      <Text>Hey I'm {selectedChatFaceData.name}</Text>
 
+    const onFacePress=(id) =>{
+      setSelectedChatFaceData(chatFaceData[id])
+    }
+  return ( 
+     <View style={{alignItems:'center',paddingTop:90}}>
+      <Text style={[{color:selectedChatFaceData.primary}, {fontSize:30}]}>Hey</Text>
+      <Text style={[{color:selectedChatFaceData.primary}, {fontSize:30, fontWeight:'bold'}]}>I'm {selectedChatFaceData.name}</Text>
+
+
+      <Image source={{uri:selectedChatFaceData.image}}
+      style={{width:150,height:150,marginTop:20}} />
+      <Text style={{marginTop:30,fontSize:25}}> How can I help you today?</Text>
+      <View style={{marginTop:20,backgroundColor:'#F5F5F5', alignItems:'center',height:110,padding:10,borderRadius:10 }}>
+        <FlatList
+          data={chatFaceData}
+          horizontal = {true}
+          // selectedChatFaceData.id!=item.id is to not include selectedFace in renderItem 
+          renderItem={({item})=>selectedChatFaceData.id!=item.id&&(
+            <TouchableOpacity onPress={()=>onFacePress(item.id)} style={{margin:15}}>
+              <Image source={{uri:item.image}} style={{width:40,height:40}}/>
+            </TouchableOpacity>
+          )}/>
+      <Text style={{marginTop:5,fontSize:17,color:'#B0B0B0 '}}> Choose your buddy</Text>
+       </View>
+       <TouchableOpacity style={[{backgroundColor:selectedChatFaceData.primary},
+      {padding:17,width:Dimensions.get('screen').width*0.6, borderRadius:100,alignItems:'center',marginTop:30}]}>
+        <Text style={{fontSize:16,color:'#fff'}}>
+        Chat!
+        </Text>
+       </TouchableOpacity>
     </View>
   )
 } 
