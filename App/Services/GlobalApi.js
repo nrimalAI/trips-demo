eb35d1d2926ccc2405b93d1ff656cb35a50c3eeb
Nitant R <54756510+nrimalAI@users.noreply.github.com>
2023-09-25 14:24:18 -0400

    api

---
+++ App/Services/GlobalApi.js
@@ -0,0 +1,21 @@
+const BASE_URL='http://192.168.1.158:3000/api/bard'
+
+const getBardApi = async (userMsg) => {
+  try {
+    const response = await fetch(`${BASE_URL}?ques=${userMsg}`);
+
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error:', error);
+    throw error;
+  }
+};
+
+export default {
+  getBardApi,
+};
