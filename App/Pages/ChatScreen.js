e237fedcbae9d33d6e82d80789eb44805f87964b
Nitant R <54756510+nrimalAI@users.noreply.github.com>
2023-09-26 13:34:49 -0400

    api setup

--- App/Pages/ChatScreen.js
+++ App/Pages/ChatScreen.js
@@ -1,7 +1,8 @@
 import { View, Text } from 'react-native'
 import React, { useEffect, useCallback, useState } from 'react'
 import { useRoute } from '@react-navigation/native'
-import { GiftedChat   } from 'react-native-gifted-chat';
+import { Bubble, GiftedChat, InputToolbar, Send } from 'react-native-gifted-chat'
+import GlobalApi from '../Services/GlobalApi';
 
 export default function ChatScreen() {
     const param = useRoute().params; 
@@ -28,7 +29,16 @@ export default function ChatScreen() {
       setMessages(previousMessages =>
         GiftedChat.append(previousMessages, messages),
       )
+      if(messages[0].text){
+        getChatResp(messages[0].text)
+      }
     }, [])
+
+    const getChatResp=(msg)=>{
+        GlobalApi.getBardApi(msg).then(rsp=>{
+            console.log(rsp);
+        })
+    }
        
   return (
     <View style={{flex:1, backgroundColor:'#fff'}}>
